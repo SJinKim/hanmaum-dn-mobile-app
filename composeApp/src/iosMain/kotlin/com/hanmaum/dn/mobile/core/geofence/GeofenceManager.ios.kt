c62c70df2495727d9bdbcbf1628ba51e7d39e877
SJ-private <ksj_1207@hotmail.com>
2026-04-27 01:08:42 +0200

    setup: pr check added feat: left overs

--- composeApp/src/iosMain/kotlin/com/hanmaum/dn/mobile/core/geofence/GeofenceManager.ios.kt
+++ composeApp/src/iosMain/kotlin/com/hanmaum/dn/mobile/core/geofence/GeofenceManager.ios.kt
@@ -1,6 +1,7 @@
 package com.hanmaum.dn.mobile.core.geofence
 
 import com.hanmaum.dn.mobile.features.geofence.domain.model.ChurchLocation
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.CoreLocation.CLCircularRegion
 import platform.CoreLocation.CLLocationCoordinate2DMake
 import platform.CoreLocation.CLLocationManager
@@ -13,11 +14,12 @@ import platform.darwin.NSObject
 
 private const val REGION_ID = "church_geofence"
 
+@OptIn(ExperimentalForeignApi::class)
 class IosGeofenceManager : GeofenceManager {
 
     private val manager = CLLocationManager()
 
-    @Volatile
+    // Kotlin/Native new memory model: shared mutable state is safe; @Volatile is JVM-only
     private var onEnterCallback: (() -> Unit)? = null
 
     private val delegate = object : NSObject(), CLLocationManagerDelegateProtocol {
