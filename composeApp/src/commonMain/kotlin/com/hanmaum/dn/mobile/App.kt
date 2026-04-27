231ef08d66688ffb16e10dc8140a9929e3b76c5c
SJ-private <ksj_1207@hotmail.com>
2026-04-28 00:14:24 +0200

    feat(nav): wire BottomNavBar into App with Scaffold and conditional visibility

--- composeApp/src/commonMain/kotlin/com/hanmaum/dn/mobile/App.kt
+++ composeApp/src/commonMain/kotlin/com/hanmaum/dn/mobile/App.kt
@@ -1,13 +1,19 @@
 package com.hanmaum.dn.mobile
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
-import com.hanmaum.dn.mobile.core.presentation.theme.AppTheme
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.hanmaum.dn.mobile.core.domain.model.NavRoute
 import com.hanmaum.dn.mobile.core.navigation.*
+import com.hanmaum.dn.mobile.core.presentation.components.BottomNavBar
+import com.hanmaum.dn.mobile.core.presentation.theme.AppTheme
 import com.hanmaum.dn.mobile.features.announcement.presentation.AnnouncementDetailScreen
 import com.hanmaum.dn.mobile.features.announcement.presentation.AnnouncementListScreen
 import com.hanmaum.dn.mobile.features.announcement.presentation.HomeScreen
@@ -26,136 +32,162 @@ fun App() {
     KoinContext {
         AppTheme {
             val navController = rememberNavController()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
 
-            NavHost(
-                navController = navController,
-                startDestination = SplashRoute,
-            ) {
-                composable<SplashRoute> {
-                    SplashScreen(
-                        onNavigate = { route ->
-                            val targetRoute: Any = when (route) {
-                                NavRoute.Home            -> HomeRoute
-                                NavRoute.Login           -> LoginRoute
-                                NavRoute.PendingApproval -> PendingRoute
+            val showBottomBar = TopLevelDestination.all.any { dest ->
+                currentDestination?.hasRoute(dest.routeClass) == true
+            }
+
+            Scaffold(
+                bottomBar = {
+                    if (showBottomBar) {
+                        BottomNavBar(
+                            currentDestination = currentDestination,
+                            onDestinationSelected = { dest ->
+                                navController.navigate(dest.routeInstance) {
+                                    popUpTo<HomeRoute> {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+                                    restoreState = true
+                                }
+                            },
+                        )
+                    }
+                },
+            ) { innerPadding ->
+                NavHost(
+                    navController = navController,
+                    startDestination = SplashRoute,
+                    modifier = Modifier.padding(innerPadding),
+                ) {
+                    composable<SplashRoute> {
+                        SplashScreen(
+                            onNavigate = { route ->
+                                val targetRoute: Any = when (route) {
+                                    NavRoute.Home            -> HomeRoute
+                                    NavRoute.Login           -> LoginRoute
+                                    NavRoute.PendingApproval -> PendingRoute
+                                }
+                                navController.navigate(targetRoute) {
+                                    popUpTo<SplashRoute> { inclusive = true }
+                                }
                             }
-                            navController.navigate(targetRoute) {
-                                popUpTo<SplashRoute> { inclusive = true }
-                            }
-                        }
-                    )
-                }
+                        )
+                    }
 
-                composable<LoginRoute> {
-                    LoginScreen(
-                        onNavigateToHome = {
-                            navController.navigate(HomeRoute) {
-                                popUpTo<LoginRoute> { inclusive = true }
-                            }
-                        },
-                        onNavigateToPending = {
-                            navController.navigate(PendingRoute) {
-                                popUpTo<LoginRoute> { inclusive = true }
-                            }
-                        },
-                        onRegisterClick = { navController.navigate(RegisterRoute) },
-                    )
-                }
+                    composable<LoginRoute> {
+                        LoginScreen(
+                            onNavigateToHome = {
+                                navController.navigate(HomeRoute) {
+                                    popUpTo<LoginRoute> { inclusive = true }
+                                }
+                            },
+                            onNavigateToPending = {
+                                navController.navigate(PendingRoute) {
+                                    popUpTo<LoginRoute> { inclusive = true }
+                                }
+                            },
+                            onRegisterClick = { navController.navigate(RegisterRoute) },
+                        )
+                    }
 
-                composable<RegisterRoute> {
-                    RegisterScreen(
-                        onBackClick = { navController.popBackStack() },
-                        onNavigateToPending = {
-                            navController.navigate(PendingRoute) {
-                                popUpTo<LoginRoute> { inclusive = false }
-                                popUpTo<RegisterRoute> { inclusive = true }
-                            }
-                        },
-                    )
-                }
+                    composable<RegisterRoute> {
+                        RegisterScreen(
+                            onBackClick = { navController.popBackStack() },
+                            onNavigateToPending = {
+                                navController.navigate(PendingRoute) {
+                                    popUpTo<LoginRoute> { inclusive = false }
+                                    popUpTo<RegisterRoute> { inclusive = true }
+                                }
+                            },
+                        )
+                    }
 
-                composable<PendingRoute> {
-                    PendingScreen(
-                        onNavigateToHome = {
-                            navController.navigate(HomeRoute) {
-                                popUpTo<PendingRoute> { inclusive = true }
-                            }
-                        },
-                        onNavigateToLogin = {
-                            navController.navigate(LoginRoute) {
-                                popUpTo(0) { inclusive = true }
-                            }
-                        },
-                    )
-                }
+                    composable<PendingRoute> {
+                        PendingScreen(
+                            onNavigateToHome = {
+                                navController.navigate(HomeRoute) {
+                                    popUpTo<PendingRoute> { inclusive = true }
+                                }
+                            },
+                            onNavigateToLogin = {
+                                navController.navigate(LoginRoute) {
+                                    popUpTo(0) { inclusive = true }
+                                }
+                            },
+                        )
+                    }
 
-                composable<HomeRoute> {
-                    HomeScreen(
-                        onLogout = {
-                            navController.navigate(LoginRoute) {
-                                popUpTo(0) { inclusive = true }
-                            }
-                        },
-                        onAnnouncementClick = { id ->
-                            navController.navigate(AnnouncementDetailRoute(id = id))
-                        },
-                        onViewAllClick  = { navController.navigate(AnnouncementListRoute) },
-                        onProfileClick  = { navController.navigate(ProfileRoute) },
-                        onMinistryClick = { navController.navigate(MinistryListRoute) },
-                        onCommunityClick = { navController.navigate(CommunityRoute) },
-                        onNewsClick     = { navController.navigate(AnnouncementListRoute) },
-                    )
-                }
+                    composable<HomeRoute> {
+                        HomeScreen(
+                            onLogout = {
+                                navController.navigate(LoginRoute) {
+                                    popUpTo(0) { inclusive = true }
+                                }
+                            },
+                            onAnnouncementClick = { id ->
+                                navController.navigate(AnnouncementDetailRoute(id = id))
+                            },
+                            onViewAllClick   = { navController.navigate(AnnouncementListRoute) },
+                            onProfileClick   = { navController.navigate(ProfileRoute) },
+                            onMinistryClick  = { navController.navigate(MinistryListRoute) },
+                            onCommunityClick = { navController.navigate(CommunityRoute) },
+                            onNewsClick      = { navController.navigate(AnnouncementListRoute) },
+                        )
+                    }
 
-                composable<AnnouncementDetailRoute> { backStackEntry ->
-                    val route: AnnouncementDetailRoute = backStackEntry.toRoute()
-                    AnnouncementDetailScreen(
-                        announcementId = route.id,
-                        onBackClick    = { navController.popBackStack() },
-                    )
-                }
+                    composable<AnnouncementDetailRoute> { backStackEntry ->
+                        val route: AnnouncementDetailRoute = backStackEntry.toRoute()
+                        AnnouncementDetailScreen(
+                            announcementId = route.id,
+                            onBackClick    = { navController.popBackStack() },
+                        )
+                    }
 
-                composable<AnnouncementListRoute> {
-                    AnnouncementListScreen(
-                        onBackClick = { navController.popBackStack() },
-                        onItemClick = { id ->
-                            navController.navigate(AnnouncementDetailRoute(id = id))
-                        },
-                    )
-                }
+                    composable<AnnouncementListRoute> {
+                        AnnouncementListScreen(
+                            onBackClick = { navController.popBackStack() },
+                            onItemClick = { id ->
+                                navController.navigate(AnnouncementDetailRoute(id = id))
+                            },
+                        )
+                    }
 
-                composable<ProfileRoute> {
-                    ProfileScreen(
-                        onBackClick = { navController.popBackStack() },
-                        onLogout = {
-                            navController.navigate(LoginRoute) {
-                                popUpTo(0) { inclusive = true }
-                            }
-                        },
-                    )
-                }
+                    composable<ProfileRoute> {
+                        ProfileScreen(
+                            onBackClick = { navController.popBackStack() },
+                            onLogout = {
+                                navController.navigate(LoginRoute) {
+                                    popUpTo(0) { inclusive = true }
+                                }
+                            },
+                        )
+                    }
 
-                composable<MinistryListRoute> {
-                    MinistryListScreen(
-                        onBackClick   = { navController.popBackStack() },
-                        onMinistryClick = { publicId ->
-                            navController.navigate(MinistryDetailRoute(publicId = publicId))
-                        },
-                    )
-                }
+                    composable<MinistryListRoute> {
+                        MinistryListScreen(
+                            onBackClick     = { navController.popBackStack() },
+                            onMinistryClick = { publicId ->
+                                navController.navigate(MinistryDetailRoute(publicId = publicId))
+                            },
+                        )
+                    }
 
-                composable<MinistryDetailRoute> { backStackEntry ->
-                    val route: MinistryDetailRoute = backStackEntry.toRoute()
-                    MinistryDetailScreen(
-                        publicId    = route.publicId,
-                        onBackClick = { navController.popBackStack() },
-                    )
-                }
+                    composable<MinistryDetailRoute> { backStackEntry ->
+                        val route: MinistryDetailRoute = backStackEntry.toRoute()
+                        MinistryDetailScreen(
+                            publicId    = route.publicId,
+                            onBackClick = { navController.popBackStack() },
+                        )
+                    }
 
-                composable<CommunityRoute> {
-                    CommunityStubScreen(
-                        onBackClick = { navController.popBackStack() },
-                    )
+                    composable<CommunityRoute> {
+                        CommunityStubScreen(
+                            onBackClick = { navController.popBackStack() },
+                        )
+                    }
                 }
             }
         }
